cf0eb9d7d05d728f78dd7c9f13ff6ddec7f703fa
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-10-31 23:23:19 -0400

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# React Movies App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-Project's data source was provided by [The Movie Database (TMDB)](https://developers.themoviedb.org/).
---
+++ precache-manifest.35dd4dd9f770d085623b464510a38c3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040865b5466a76d54cd472c087d5b88c",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "1fdd25568cda0442cab8",
+    "url": "/movies-app/static/css/main.91bc0007.chunk.css"
+  },
+  {
+    "revision": "32dc1b08d12d7d8d119c",
+    "url": "/movies-app/static/js/2.5d8531d2.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/movies-app/static/js/2.5d8531d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdd25568cda0442cab8",
+    "url": "/movies-app/static/js/main.efbdbe34.chunk.js"
+  },
+  {
+    "revision": "6f4ce7a2b201b6f9f6f8",
+    "url": "/movies-app/static/js/runtime-main.94bcfe81.js"
+  },
+  {
+    "revision": "406b8801abf0284dcee43c18388159d6",
+    "url": "/movies-app/static/media/popcorn.406b8801.png"
+  }
+]);
